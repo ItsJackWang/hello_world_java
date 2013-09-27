d4925181eff959139daaf6d36ecdfda1e261aa33
ItsJackWang <wanghaichenat2010@gmail.com>
2013-09-27 13:57:54 +0800

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         //origin change 1
         //origin chagne 2
+        //origin change 3
         System.out.print("Hello world!");
     }
 }
